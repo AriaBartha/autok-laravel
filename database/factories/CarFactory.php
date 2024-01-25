f15f63fe0a7037a341d0c667d0f11ef23de56baa
Bartha Ariana <ariabartha@gmail.com>
2024-01-25 15:01:17 +0100

    Seeder létrehozva tesztadatok generálására

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -17,7 +17,11 @@ class CarFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'license_plate_number' => fake()->bothify('???-###'),
+            'brand' => fake()->word(),
+            'model' => fake()->word(),
+            'year_of_manufacture' => fake()->year(),
+            'fuel_type' => fake()->word(),
         ];
     }
 }
